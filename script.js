c9f959124ca0a2262114405d876dd0e5d4dbf863
bovaqulov <bovaqulov@gmail.com>
2024-02-10 18:45:53 +0500

    'test'

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = prompt("Ismingizni kiriting:");
+console.log("Sizning ismingiz: " + name);
+
+var age = prompt("Yoshingizni kiriting:");
+console.log("Sizning yoshingiz: " + age);
+
+var misol = prompt("Misolni yeching 20 + 5 = ?");
+var misol = prompt("Misolni yeching 12 + 3 = ?");
+var misol = prompt("Misolni yeching 4 * 6 = ?");
+var misol = prompt("Misolni yeching 63 / 3 = ?");
+var misol = prompt("Misolni yeching 54 - 7 = ?");
+var result = prompt("Natijani ko'rish uchun consoleni bosing.");
+
+console.log("Misol 1: 10 + 25 = 35" + " Sizning javobingiz: " + misol);
+console.log("Misol 2: 9 + 3 = 12" + " Sizning javobingiz: " + misol);
+console.log("Misol 3: 4 * 4 = 16" + " Sizning javobingiz: " + misol);
+console.log("Misol 4: 9 / 3 = 3" + " Sizning javobingiz: " + misol);
+
+
+
+
+
